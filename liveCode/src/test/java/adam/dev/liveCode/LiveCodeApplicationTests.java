bcc2cdf6edbf87109dea5669c517471f1cdc5f41
adam-sion <adamsion74@gmail.com>
2024-12-16 14:19:28 +0200

    feat: add springBoot fileStructure

---
+++ liveCode/src/test/java/adam/dev/liveCode/LiveCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package adam.dev.liveCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
