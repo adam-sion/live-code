078c9aa183f4a3827c8a959d7a2998bf527ecb45
adam-sion <adamsion74@gmail.com>
2024-12-24 00:58:59 +0200

    feat: add code_line_operation entity

--- db/DDL.sql
+++ db/DDL.sql
@@ -1,23 +1,19 @@
-
 DROP SCHEMA IF EXISTS live_code CASCADE;
 CREATE SCHEMA live_code;
-
 
 DROP TABLE IF EXISTS live_code.users CASCADE;
 CREATE TABLE live_code.users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
-	password VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL
 );
-
 
 DROP TABLE IF EXISTS live_code.rooms CASCADE;
 CREATE TABLE live_code.rooms (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL
 );
-
 
 DROP TABLE IF EXISTS live_code.room_user CASCADE;
 CREATE TABLE live_code.room_user (
@@ -27,7 +23,6 @@ CREATE TABLE live_code.room_user (
     PRIMARY KEY (room_id, user_id)
 );
 
-
 DROP TABLE IF EXISTS live_code.active_room_user CASCADE;
 CREATE TABLE live_code.active_room_user (
     room_id BIGINT NOT NULL REFERENCES live_code.rooms(id) ON DELETE CASCADE,
@@ -35,7 +30,6 @@ CREATE TABLE live_code.active_room_user (
     joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (room_id, user_id)
 );
-
 
 DROP TABLE IF EXISTS live_code.room_user_requests CASCADE;
 CREATE TABLE live_code.room_user_requests (
@@ -45,14 +39,12 @@ CREATE TABLE live_code.room_user_requests (
     status VARCHAR(20) CHECK(status IN ('pending', 'accepted', 'declined')) DEFAULT 'pending'
 );
 
-
 DROP TABLE IF EXISTS live_code.room_code CASCADE;
 CREATE TABLE live_code.room_code (
     id SERIAL PRIMARY KEY,
     room_id BIGINT NOT NULL REFERENCES live_code.rooms(id) ON DELETE CASCADE,
     language VARCHAR(50) NOT NULL
 );
-
 
 DROP TABLE IF EXISTS live_code.code_line_details CASCADE;
 CREATE TABLE live_code.code_line_details (
@@ -61,12 +53,19 @@ CREATE TABLE live_code.code_line_details (
     line_number INT NOT NULL
 );
 
--- Create the 'code_line' table to store lines of code linked to room_code and users
 DROP TABLE IF EXISTS live_code.code_line CASCADE;
 CREATE TABLE live_code.code_line (
     id SERIAL PRIMARY KEY,
     room_code_id BIGINT NOT NULL REFERENCES live_code.room_code(id) ON DELETE CASCADE,
-    user_id BIGINT NOT NULL REFERENCES live_code.users(id) ON DELETE CASCADE,
     code_line_details_id BIGINT NOT NULL REFERENCES live_code.code_line_details(id) ON DELETE CASCADE,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS live_code.code_line_operation CASCADE;
+CREATE TABLE live_code.code_line_operation (
+    id SERIAL PRIMARY KEY,
+    code_line_id BIGINT NOT NULL REFERENCES live_code.code_line(id) ON DELETE CASCADE,
+    user_id BIGINT NOT NULL REFERENCES live_code.users(id) ON DELETE CASCADE,
+    operation_type VARCHAR(50) CHECK(operation_type IN ('insert', 'update', 'delete')) NOT NULL,
+    new_content TEXT 
 );
