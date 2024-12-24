1e7640051fbd59add0bcf665616f635f5b5d7678
adam-sion <adamsion74@gmail.com>
2024-12-24 10:12:53 +0200

    feat: ws done

--- back/liveCode/src/main/java/adam/dev/liveCode/controller/DefaultController.java
+++ back/liveCode/src/main/java/adam/dev/liveCode/controller/DefaultController.java
@@ -1,17 +1,17 @@
-package adam.dev.liveCode.controller;
-
-import adam.dev.liveCode.exception.ErrorResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class DefaultController {
-
-    @RequestMapping("/**")
-    public ResponseEntity<ErrorResponse> handleInvalidEndpoint() {
-        return new ResponseEntity<>(new ErrorResponse("Not found", "endpoint not found"), HttpStatus.NOT_FOUND);  // Correct order
-    }
-}
-
+//package adam.dev.liveCode.controller;
+//
+//import adam.dev.liveCode.exception.ErrorResponse;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class DefaultController {
+//
+//    @RequestMapping("/**")
+//    public ResponseEntity<ErrorResponse> handleInvalidEndpoint() {
+//        return new ResponseEntity<>(new ErrorResponse("Not found", "endpoint not found"), HttpStatus.NOT_FOUND);  // Correct order
+//    }
+//}
+//
