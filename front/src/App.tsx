c74829c77a0c5dbe028005bceb0d9552eaf6450c
adam-sion <adamsion74@gmail.com>
2024-12-28 23:19:12 +0200

    feat: auth works front and back

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,14 +4,18 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Routes.tsx'
 import { AuthProvider } from './contexts/AuthContext.tsx'
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App:FC = ()=> {
   return (
+    <>
     <AuthProvider>
     <RouterProvider router={router}>
     </RouterProvider>
     </AuthProvider>
-   
+    <ToastContainer position='bottom-right' limit={2} autoClose={1500}/>
+   </>
   )
 }
 
