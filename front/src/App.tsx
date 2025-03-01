2507702a45da930207fe58de75ab7f40ffbd0090
Adam <adamsion74@gmail.com>
2025-03-01 09:12:20 +0200

    fix: eslint warn unued

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import './App.css'
 import './index.css'
 import {RouterProvider } from 'react-router-dom';
-import { router } from './routes/Routes.tsx';
+import { router } from '../src/routes/Routes.tsx';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
